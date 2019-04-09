006137997708cb8029b19dfe9c94e0a844b97728
Miguel Faria <miguel_dasilva90@hotmail.com>
2019-04-09 13:46:58 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/miguelfaria/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.miguelfaria.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.miguelfaria.cursomc.domain.Cliente;
+import com.miguelfaria.cursomc.repositories.ClienteRepository;
+import com.miguelfaria.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado ! Id: " + id 
+					+ ",Tipo:" + Cliente.class.getName());
+		}
+		return obj;
+	}
+}
+
+	
