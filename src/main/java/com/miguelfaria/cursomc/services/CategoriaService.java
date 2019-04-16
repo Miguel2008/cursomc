c66007b2642e76a941a90a5ea4cb7ee916baff59
Miguel Faria <miguel_dasilva90@hotmail.com>
2019-04-16 11:06:03 -0300

    Validacao sintática com Bean Validation

--- src/main/java/com/miguelfaria/cursomc/services/CategoriaService.java
+++ src/main/java/com/miguelfaria/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.miguelfaria.cursomc.domain.Categoria;
+import com.miguelfaria.cursomc.dto.CategoriaDTO;
 import com.miguelfaria.cursomc.repositories.CategoriaRepository;
 import com.miguelfaria.cursomc.services.exceptions.DataIntegrityException;
 import com.miguelfaria.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,8 +56,13 @@ public class CategoriaService {
 	
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageResquest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		return repo.findAll(pageResquest);
-		
+		return repo.findAll(pageResquest);	
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	
 
 }	
